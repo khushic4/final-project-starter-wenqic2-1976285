290199b68504a43d9e316ec73e83aadea0cf86ac
huangpar <parkerdhuang@gmail.com>
2022-02-18 18:49:30 -0800

    Updated graph

--- source/Graph-1.R
+++ source/Graph-1.R
@@ -10,17 +10,18 @@ HIVdata <- HIVdata[-1,]
 hivdata_2 <- HIVdata%>%
   pivot_longer(HIVdata, cols=2:32, names_to = "year", values_to = "Total_People_Living_With_HIV")
   
-  
+
 hivdata <- hivdata_2 %>%
   filter(`WHO region` == "Global")%>%
-  mutate(people_with_hiv = gsub("\\[.*" ,"",hivdata$Total_People_Living_With_HIV))
+  mutate(people_with_hiv = gsub("\\[.*" ,"",hivdata$Total_People_Living_With_HIV))%>%
+  mutate(people_with_hiv = gsub(" ", "", hivdata$people_with_hiv))
 
 hivdata$year <- as.numeric(hivdata$year)
-hivdata$people_with_hiv <- as.factor(hivdata$people_with_hiv)
 hivdata$people_with_hiv <- as.numeric(hivdata$people_with_hiv)
 
 # Graph showing the number of cases in world each year
-ggplot(hivdata, aes(year, people_with_hiv)) + geom_point() + xlim(1992,2020)+
+ggplot(hivdata, aes(year, people_with_hiv)) + geom_line() + 
+  scale_y_continuous(labels = scales::comma)+
   labs(x = "Year", y = "Number of people living with HIV", title = "Total People Living With HIV Each Year")
 
 
